1006ab8ab2db03a77f5c20466f074678b063ba80
Sasha Aickin <xander76@yahoo.com>
2015-07-21 09:24:05 -0700

    Improved error handling on the JavaScript compilation step.

--- src/startServer.js
+++ src/startServer.js
@@ -36,13 +36,9 @@ export default function(routesRelativePath, {
 	if (compileOnly) {
 		logger.notice("Starting compilation of client JavaScript...");
 		compiler.run((err, stats) => {
-		    if(err) {
-		    	logger.error("Error during compilation.");
-		    	logger.error(err);
-		    	// TODO: inspect stats to see if there are errors -sra.
-		    } else {
-			    logger.notice("Successfully compiled client JavaScript.");
-			}
+			handleCompilationErrors(err, stats);
+			
+			logger.notice("Successfully compiled client JavaScript.");
 		});
 	} else {
 		const startJsServer = hot ? startHotLoadJsServer : startStaticJsServer;
@@ -52,7 +48,10 @@ export default function(routesRelativePath, {
 			jsUrl ? Promise.resolve() : startJsServer(compiler, jsPort),
 			startHtmlServer(serverRoutes, port)
 		])
-			.then(() => logger.notice(`Ready for requests on port ${port}.`));
+			.then(
+				() => logger.notice(`Ready for requests on port ${port}.`),
+				() => process.exit(1)
+			);
 	}
 }
 
@@ -71,23 +70,18 @@ const startHtmlServer = (serverRoutes, port) => {
 const startStaticJsServer = (compiler, port) => {
 	return new Promise((resolve, reject) => {
 		compiler.run(function(err, stats) {
-		    if(err) {
-		    	logger.error("Error during webpack build.");
-		    	logger.error(err);
-		    	reject(err);
-		    	// TODO: inspect stats to see if there are errors -sra.
-		    } else {
-			    logger.debug("Successfully compiled static JavaScript.");
-	    		// TODO: make this parameterized based on what is returned from compileClient
-	    		let server = express();
-				server.use('/', compression(), express.static('__clientTemp/build'));
-				logger.info("Starting static JavaScript server...");
+			handleCompilationErrors(err, stats);
 
-				http.createServer(server).listen(port, function() {
-					logger.info(`Started static JavaScript server on port ${port}`);
-					resolve();
-				});
-		    }
+		    logger.debug("Successfully compiled static JavaScript.");
+    		// TODO: make this parameterized based on what is returned from compileClient
+    		let server = express();
+			server.use('/', compression(), express.static('__clientTemp/build'));
+			logger.info("Starting static JavaScript server...");
+
+			http.createServer(server).listen(port, function() {
+				logger.info(`Started static JavaScript server on port ${port}`);
+				resolve();
+			});
 		});
 	});
 };
@@ -106,3 +100,23 @@ const startHotLoadJsServer = (compiler, port) => {
 	return Promise.all([compiledPromise, serverStartedPromise])
 		.then(() => logger.info(`Started hot reload JavaScript server on port ${port}`));
 };
+
+const handleCompilationErrors = (err, stats) => {
+	if(err) {
+    	logger.error("Error during webpack build.");
+    	logger.error(err);
+    	throw new Error(err);
+    	// TODO: inspect stats to see if there are errors -sra.
+    } else if (stats.hasErrors()) {
+    	logger.error("There were errors in the JavaScript compilation.");
+    	stats.toJson().errors.forEach((error) => {
+    		logger.error(error);
+    	});
+    	throw new Error("There were errors in the JavaScript compilation.");
+    } else if (stats.hasWarnings()) {
+    	logger.warning("There were warnings in the JavaScript compilation. Note that this is normal if you are minifying your code.");
+    	// for now, don't enumerate warnings; they are absolutely useless in minification mode.
+    	// TODO: handle this more intelligently, perhaps with a --reportwarnings flag or with different
+    	// behavior based on whether or not --minify is set.
+    }
+}
